c3c03afdb6a4c2414bcbc0c405af7a297492b659
Hasmukh <hasmukhrathod@gmail.com>
2018-12-30 11:36:20 +0000

    faker & database_cleaner setup done

--- Gemfile
+++ Gemfile
@@ -41,6 +41,11 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
   gem 'factory_bot_rails'
+  gem 'faker'
+end
+
+group :test do
+  gem 'database_cleaner'
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,4 +59,18 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+  config.after(:suite) do
+    DatabaseCleaner.clean
+  end
+  
 end
