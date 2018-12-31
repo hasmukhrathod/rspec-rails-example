887f906bedd64f01a20186eb4a01ec74b944c99c
Hasmukh <hasmukhrathod@gmail.com>
2018-12-31 12:05:07 +0000

    validation added for List class.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   describe "ActiveRecord Association" do
     let(:item) {create(:item)}
-    it {item.should belong_to(:list)}
+    it {should belong_to(:list)}
   end
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  it 'has a valid factory' do
-    build(:list).should be_valid
+  describe 'Validations' do
+    subject {build(:list)}
+    it 'should be a valid object' do
+      should be_valid 
+    end
+    
+    ##it will check this validation in Model -> it has nothing to do with factory right now.
+    it 'must have a name' do
+      should validate_presence_of(:name)
+    end
+    it 'must have a unique name' do
+      should validate_uniqueness_of(:name)
+    end
   end
 end
