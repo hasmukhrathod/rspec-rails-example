5f3fa8fca2e75e82f295e07a237ad6b0186f5068
Hasmukh <hasmukhrathod@gmail.com>
2019-01-03 02:34:54 +0000

    subject {Class.new} -> should, is_expected

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  
+  describe "should not valid without List" do
+    it {should_not be_valid}
+  end
+  describe "should be valid object" do
+    subject {(build_stubbed(:item))}
+    it {is_expected.to be_valid}
+  end
+  
   describe "ActiveRecord Association" do
     let(:item) {create(:item)}
-    it {should belong_to(:list)}
+    it {expect(item).to belong_to(:list)}
   end
 end
